73d18364f99428bb5697e59095824d7b613cf2fe
Christian Ingwersen <cin@trackman.com>
2023-01-29 10:21:30 +0100

    3dpw metrics

--- src/sportspose/utils/datautils.py
+++ src/sportspose/utils/datautils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import glob
 import os
 import numpy as np
+import pickle
 
 
 def get_total_capture_sequence_dist(data_path):
@@ -90,11 +91,45 @@ def compute_frames_under_cm_threshold(sequence_movements, threshold=10):
 
     return base_frames, frames
 
+
 def get_totalcapture_table_metrics():
     sequence_movement = get_total_capture_sequence_dist("data/totalcapture")
     frames = compute_frames_under_cm_threshold(sequence_movement, threshold=5)
     frames = compute_frames_under_cm_threshold(sequence_movement, threshold=10)
 
-    
+
+def get_3dpw_sequence_dist(data_path):
+    """
+    Get the 3DPW data
+    Then compute the pairwise euclidean distance between each point in the rows in the array.
+    :param data_path: path to the 3DPW capture data
+    :return: 3DPW data
+    """
+    pickle_files = glob.glob(os.path.join(data_path, "**", "*.pkl"))
+    sequence_movements = []
+    for pickle_file in pickle_files:
+        with open(pickle_file, "rb") as f:
+            data = pickle.load(f, encoding="latin1")
+        joints = data["jointPositions"][0].reshape(-1, 24, 3)
+
+        # Compute pairwise euclidean distance between each point in the rows in the array
+        sequence_dist = np.linalg.norm(
+            joints[1:, :, :] - joints[:-1, :, :], axis=2
+        ).mean(axis=1)
+
+        # Convert to cm from m
+        sequence_dist = sequence_dist * 100
+
+        sequence_movements.append(sequence_dist)
+
+    return sequence_movements
+
+
+def get_3dpw_table_metrics():
+    sequence_movement = get_3dpw_sequence_dist("data/3dpw")
+    frames = compute_frames_under_cm_threshold(sequence_movement, threshold=5)
+    frames = compute_frames_under_cm_threshold(sequence_movement, threshold=10)
+
+
 if __name__ == "__main__":
-    
+    get_3dpw_table_metrics()
